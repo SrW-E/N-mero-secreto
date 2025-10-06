cba3526147d420a2681aee55aaf04b39783f878b
SrW-E <edwardgabrielcamargo@gmail.com>
2025-10-06 15:00:28 -0300

    again

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 15;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 function exibirTexto(tag,texto){
@@ -9,7 +9,7 @@ function exibirTexto(tag,texto){
 }
 function textoInicial(){
 exibirTexto("h1","Jogo do número secreto");
-exibirTexto("p","Escolha um número entre 1 e 10 :");
+exibirTexto("p","Escolha um número entre 1 e 15 :");
 }
 textoInicial();
 
